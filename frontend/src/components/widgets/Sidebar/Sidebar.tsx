30a752d6ac9c97f78d5560d1892f7311bb04f53d
Mortis <40495603+Mortgu@users.noreply.github.com>
2022-10-27 20:20:39 +0200

    Added Dropdowns

--- frontend/src/components/widgets/Sidebar/Sidebar.tsx
+++ frontend/src/components/widgets/Sidebar/Sidebar.tsx
@@ -2,10 +2,10 @@ import "./Sidebar.scss";
 import {NavLink} from "react-router-dom";
 
 import ClockIcon from "@material-ui/icons/Schedule";
-import NoteIcon from "@material-ui/icons/NoteOutlined";
-import AddIcon from "@material-ui/icons/Add";
 import PersonIcon from "@material-ui/icons/PersonOutlined";
 import OrganisationIcon from "@material-ui/icons/HouseOutlined";
+import SidebarCTA from "./elements/SidebarCTA";
+import NoteIcon from "@material-ui/icons/NoteOutlined";
 
 const Sidebar = () => {
     return (
@@ -15,14 +15,11 @@ const Sidebar = () => {
                     <ClockIcon className="icon" />
                     <p>Recents</p>
                 </NavLink>
-                <NavLink to="/subjects" data-active="false" className="sidebar-cta">
-                    <NoteIcon className="icon" />
-                    <p>Drafts</p>
-                    <div className="on-hover-cta">
-                        <AddIcon className="icon" />
-                    </div>
-                    <div className="dropdown" data-open="false"></div>
-                </NavLink>
+                <SidebarCTA
+                    icon={<NoteIcon className="icon" />}
+                    text="Drafts"
+                    link="/subjects"
+                />
             </section>
             <div className="sidebar-separator"></div>
             <section className="sidebar-section">
@@ -30,13 +27,11 @@ const Sidebar = () => {
                     <OrganisationIcon className="icon" />
                     <p>Organisation</p>
                 </NavLink>
-                <NavLink to="/members" className="sidebar-cta">
-                    <PersonIcon className="icon" />
-                    <p>Members</p>
-                    <div className="on-hover-cta">
-                        <AddIcon className="icon" />
-                    </div>
-                </NavLink>
+                <SidebarCTA
+                    icon={<PersonIcon className="icon" />}
+                    text="Members"
+                    link="/members"
+                />
             </section>
         </div>
     )
---
+++ frontend/src/components/widgets/Sidebar/elements/SidebarCTA.tsx
@@ -0,0 +1,46 @@
+import {useState} from "react";
+
+import {NavLink} from "react-router-dom";
+import AddIcon from "@material-ui/icons/Add";
+import SendIcon from "@material-ui/icons/Send";
+
+import {useClickHook} from "../../../../hooks/useClickHook";
+
+const SidebarCTA = ({icon, text, link}: any) => {
+    const { useOutsideClick } = useClickHook();
+
+    const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+
+    const handleAddButton = (event: any) => {
+        setDropdownIsOpen(true);
+
+    }
+
+    const hideDropdown = () => {
+        setDropdownIsOpen(false);
+    }
+
+    const ref = useOutsideClick(hideDropdown);
+
+    console.log(ref)
+
+    return (
+        <NavLink ref={ref} to={link} data-active={dropdownIsOpen} className="sidebar-cta">
+            {icon}
+            <p>{text}</p>
+            <div className="on-hover-cta" onClick={handleAddButton}>
+                <AddIcon className="icon" />
+            </div>
+            <div className="dropdown" data-open={dropdownIsOpen}>
+                <div className="input-buttonIcon">
+                    <input  ref={input => input && input.focus()}  className="input" type="text" placeholder="Enter Name..." />
+                    <button>
+                        <SendIcon />
+                    </button>
+                </div>
+            </div>
+        </NavLink>
+    )
+}
+
+export default SidebarCTA;
